c96814f44fbd946e10d5e19b435f02bbbaf36b0e
ReahKy23 <Kylah.kerr23@gmail.com>
2025-11-17 16:53:11 -0500

    week 9

---
+++ class-demos/week_09/class_09_demo/src/App.vue
@@ -0,0 +1,34 @@
+<script setup>
+// this is javascript code
+// imports, tell the library to pull something specific to vue
+// this is a function that allows us to do something when the vue loads on the webpage
+import { onMounted} from "vue";
+import MyButton from "./MyButton.vue";
+
+
+// onMounted is similar to window.onload()
+onMounted(() => {
+  console.log("the component has mounted, or added to the DOM");
+});
+
+// const buttonClick = () =>{
+
+// }
+
+</script>
+
+<template>
+  <!-- this is html code -->
+  <!-- the '@' symbol is shorthand for 'v-on', which attaches an event listener to an element -->
+
+  <MyButton />
+  <img src="./public/imgs/1x/screen.png">
+</template>
+
+<style scoped>
+/* this is css  */
+
+button {
+  border: 3px solid cyan;
+}
+</style>
---
+++ class-demos/week_09/class_09_demo/src/MyButton.vue
@@ -0,0 +1,23 @@
+<script setup>
+import{ref} from 'vue'
+// declare a varible in vue using "ref()"
+// this is a number equal to 0
+let numClicks = ref(0);
+
+function buttonClick() {
+  console.log("click detected");
+
+  // value needs to be used in order to acces teh value of teh variable in the <script>
+  numClicks.value++;
+}
+</script>
+
+<template>
+  <button @click="buttonClick">click me</button>
+  <div>
+    number of clicks {{ numClicks }}
+  </div>
+</template>
+
+<style scoped>
+</style>
